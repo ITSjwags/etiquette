61de8114d3e51b4b7b56c61a19945af7c27d785d
ITSjwags <jvwagoner@gmail.com>
2020-01-10 20:28:17 -0800

    Add styling packages and set up some base styling

---
+++ src/theme.js
@@ -0,0 +1,12 @@
+const theme = {
+  colors: {
+    blue: '#029EEB',
+    brown: '#CDA98B',
+    green: '#78EE67',
+    navy: '#090F34',
+    red: '#F7203A',
+    white: '#FFFFFF',
+  },
+}
+
+export default theme
