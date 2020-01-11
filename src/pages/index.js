bebe7db2c5d29c23732099225b98bedbd2c66b40
ITSjwags <jvwagoner@gmail.com>
2020-01-10 22:39:08 -0800

    WIP - start to set up message container

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,11 +9,36 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
 
-    <ImageContainer>
-      <Image />
-    </ImageContainer>
+    <Box>
+      <Title>
+        ‘Tis The Season To Small Talk With Your Significant Other’s Co-Workers
+      </Title>
+      <ImageContainer>
+        <Image />
+      </ImageContainer>
+    </Box>
   </Layout>
 )
+
+const Box = styled.div`
+  border: 1px solid ${({ theme }) => theme.colors.green};
+  margin: 50px auto 0;
+  padding: 35px;
+  position: relative;
+  text-align: center;
+  max-width: 1086px;
+`
+
+const Title = styled.h2`
+  background: ${({ theme }) => theme.colors.navy};
+  color: ${({ theme }) => theme.colors.green};
+  display: inline-block;
+  font-size: 28px;
+  padding: 0 35px;
+  position: relative;
+  text-align: center;
+  top: calc(-13px + -35px);
+`
 
 const ImageContainer = styled.div`
   margin: 0 auto;
